4923d0430b5ba8571589f41c40fe3e32568d98f8
Brian17256 <hernandezbrian4321@gmail.com>
2026-05-04 10:14:59 -0300

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,27 @@
-const CACHE_NAME = 'habitflow-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './icon.png'
-];
+const CACHE_NAME = 'habitflow-auto-update';
 
+// Instalación: Forzamos a que el nuevo Service Worker tome el mando
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
 });
 
+// Activación: Limpieza y control inmediato
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+// Estrategia: Intentar siempre descargar de internet para estar actualizado
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request)
+      .then((res) => {
+        // Si hay respuesta exitosa, guardamos una copia en el caché
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(() => caches.match(e.request)) // Si no hay internet, usa el caché
   );
 });
